5cb67d38045e5847056cc033bd7ff4564784ca74
julianabc3 <julianabcarvalho3@gmail.com>
2025-03-25 13:49:15 +0000

    Adicionar função skillsMember com lista de habilidades e níveis

---
+++ member.js
@@ -0,0 +1,42 @@
+function skillsMember() {
+  return {
+    "skills": [
+      {
+        "name": "HTML",
+        "level": "Intermediate"
+      },
+      {
+        "name": "CSS",
+        "level": "Intermediate"
+      },
+      {
+        "name": "JavaScript",
+        "level": "Intermediate"
+      },
+      {
+        "name": "React",
+        "level": "Intermediate"
+      },
+      {
+        "name": "Node.js",
+        "level": "Beginner"
+      },
+      {
+        "name": "Express",
+        "level": "Beginner"
+      },
+      {
+        "name": "MongoDB",
+        "level": "Beginner"
+      },
+      {
+        "name": "SQL",
+        "level": "Beginner"
+      },
+      {
+        "name": "Python",
+        "level": "Beginner"
+      }
+    ]
+  }
+}
